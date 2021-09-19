68ecdf5e396663c040a9625902be1c09baf97eea
Orochi <ali.fam0079@gmail.com>
2021-09-19 16:55:21 +0430

    Data Structure Coding Challenges 1-4

--- (03) Data Structure/Coding Challenge/script.js
+++ (03) Data Structure/Coding Challenge/script.js
@@ -1,7 +1,8 @@
 "use strict"
+
+/*
 ///////////////////////////////////////
 // Coding Challenge #1
-/*
 We're building a football betting app (soccer for my American friends 😅)!
 
 Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
@@ -83,10 +84,12 @@ printGoals(...game.scored);
 team1 < team2 && console.log("Team 1 is more likely to win");
 team2 < team1 && console.log("Team 2 is more likely to win");
 
+
+
+/*
 ///////////////////////////////////////
 // Coding Challenge #2
 
-/*
 Let's continue with our football betting app!
 
 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
@@ -127,8 +130,120 @@ const scorers = {};
 for (const player of game.scored) {
     scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
+console.log(scorers);
+console.log("########### end of 2 ");
+/*
+///////////////////////////////////////
+// Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
-console.log(scorers)
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every X minutes" (keep in mind that a
+ game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+const gameEvents = new Map([
+    [17, '⚽️ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽️ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽️ GOAL'],
+    [80, '⚽️ GOAL'],
+    [92, '🔶 Yellow card'],
+]);
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+// 3.
+console.log(`An event happened every ${ 90 / gameEvents.size} Minutes `);
+// 4.
+for (const [key,value] of gameEvents) {
+    console.log(
+        `${key < 45 ? "[FIRST HALF]" : "[SECOND HALF]" } ${key} : ${value} `
+    );
+}
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+
+document.querySelector("button").addEventListener("click" , function () {
+    const text = document.querySelector('textarea').value;
+    const eachLineArray = text.split("\n");
+
+    let counter = 1;
+    for (let curr of eachLineArray) {
+        const [word1 , word2] = curr.trim().toLowerCase().split("_");
+        curr =  word1 + word2.replace(word2[0] , word2[0].toUpperCase());
+        curr = curr.padEnd(20, " ");
+        console.log(`${curr} ${"✅".repeat(counter)}`);
+        counter++;
+
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
